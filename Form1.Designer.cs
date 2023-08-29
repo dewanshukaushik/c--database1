a4ec883f866877b32d16d27fb587de806e49cdba
Dewanshu kaushik <109568604+dewanshukaushik@users.noreply.github.com>
2023-08-30 00:00:55 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace database1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.statecomb = new System.Windows.Forms.ComboBox();
+            this.scom = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbmale = new System.Windows.Forms.RadioButton();
+            this.rbfemale = new System.Windows.Forms.RadioButton();
+            this.txtfather = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtrollno = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnexecute = new System.Windows.Forms.Button();
+            this.txtquery = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.refresh = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.statecomb);
+            this.groupBox1.Controls.Add(this.scom);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.rbmale);
+            this.groupBox1.Controls.Add(this.rbfemale);
+            this.groupBox1.Controls.Add(this.txtfather);
+            this.groupBox1.Controls.Add(this.txtname);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtrollno);
+            this.groupBox1.Location = new System.Drawing.Point(12, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(526, 252);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.refresh);
+            this.groupBox2.Controls.Add(this.btnsave);
+            this.groupBox2.Location = new System.Drawing.Point(25, 156);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(477, 79);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(396, 21);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 0;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // statecomb
+            // 
+            this.statecomb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.statecomb.FormattingEnabled = true;
+            this.statecomb.Items.AddRange(new object[] {
+            "UP",
+            "MP",
+            "HP",
+            "KARNATAKA",
+            "DELHI"});
+            this.statecomb.Location = new System.Drawing.Point(377, 126);
+            this.statecomb.Name = "statecomb";
+            this.statecomb.Size = new System.Drawing.Size(121, 24);
+            this.statecomb.TabIndex = 8;
+            this.statecomb.SelectedIndexChanged += new System.EventHandler(this.statecomb_SelectedIndexChanged);
+            // 
+            // scom
+            // 
+            this.scom.AutoSize = true;
+            this.scom.Location = new System.Drawing.Point(298, 126);
+            this.scom.Name = "scom";
+            this.scom.Size = new System.Drawing.Size(38, 16);
+            this.scom.TabIndex = 1;
+            this.scom.Text = "State";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gender";
+            // 
+            // rbmale
+            // 
+            this.rbmale.AutoSize = true;
+            this.rbmale.Location = new System.Drawing.Point(102, 118);
+            this.rbmale.Name = "rbmale";
+            this.rbmale.Size = new System.Drawing.Size(58, 20);
+            this.rbmale.TabIndex = 1;
+            this.rbmale.Text = "Male";
+            this.rbmale.UseVisualStyleBackColor = true;
+            // 
+            // rbfemale
+            // 
+            this.rbfemale.AutoSize = true;
+            this.rbfemale.Checked = true;
+            this.rbfemale.Location = new System.Drawing.Point(179, 120);
+            this.rbfemale.Name = "rbfemale";
+            this.rbfemale.Size = new System.Drawing.Size(74, 20);
+            this.rbfemale.TabIndex = 2;
+            this.rbfemale.TabStop = true;
+            this.rbfemale.Text = "Female";
+            this.rbfemale.UseVisualStyleBackColor = true;
+            // 
+            // txtfather
+            // 
+            this.txtfather.Location = new System.Drawing.Point(377, 73);
+            this.txtfather.Name = "txtfather";
+            this.txtfather.Size = new System.Drawing.Size(121, 22);
+            this.txtfather.TabIndex = 6;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(105, 73);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 22);
+            this.txtname.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Father Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Roll No";
+            // 
+            // txtrollno
+            // 
+            this.txtrollno.Location = new System.Drawing.Point(105, 35);
+            this.txtrollno.Name = "txtrollno";
+            this.txtrollno.Size = new System.Drawing.Size(100, 22);
+            this.txtrollno.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnshow);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.btnexecute);
+            this.groupBox3.Controls.Add(this.txtquery);
+            this.groupBox3.Location = new System.Drawing.Point(10, 444);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(528, 246);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            // 
+            // btnexecute
+            // 
+            this.btnexecute.Location = new System.Drawing.Point(372, 59);
+            this.btnexecute.Name = "btnexecute";
+            this.btnexecute.Size = new System.Drawing.Size(119, 23);
+            this.btnexecute.TabIndex = 2;
+            this.btnexecute.Text = "Execute Query";
+            this.btnexecute.UseVisualStyleBackColor = true;
+            this.btnexecute.Click += new System.EventHandler(this.btnexecute_Click);
+            // 
+            // txtquery
+            // 
+            this.txtquery.Location = new System.Drawing.Point(3, 21);
+            this.txtquery.Name = "txtquery";
+            this.txtquery.Size = new System.Drawing.Size(494, 22);
+            this.txtquery.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-5, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(366, 181);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(372, 104);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(119, 23);
+            this.btnshow.TabIndex = 4;
+            this.btnshow.Text = "show data";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(525, 168);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Rollno";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Father Name";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Gender";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "State";
+            this.columnHeader5.Width = 100;
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(229, 21);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(110, 23);
+            this.refresh.TabIndex = 1;
+            this.refresh.Text = "Show Detail";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 743);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label scom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbmale;
+        private System.Windows.Forms.RadioButton rbfemale;
+        private System.Windows.Forms.TextBox txtfather;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtrollno;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.ComboBox statecomb;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnexecute;
+        private System.Windows.Forms.TextBox txtquery;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace database1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SqlConnection con = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+            // con.Open();
+            // MessageBox.Show("connection is ready");
+            statecomb.SelectedIndex = 0;
+            fetchalldata();
+
+
+
+        }
+
+        private void btnexecute_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+            con.Open();
+
+
+            // MessageBox.Show("connection is ready");
+            string str = txtquery.Text;//"create table dewanshu_std(roll_no varchar(20),name varchar(50),father varchar(50), state varchar(50), gender varchar(1))";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("query Executed");
+            //drop table dewanshu_std;
+            //select * from dewanshu_std
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (checkexistance() == false)
+            {
+                if (txtrollno.Text == "" || txtname.Text == "" || txtfather.Text=="")
+                        {
+                    MessageBox.Show("please fill all the entries");
+
+                }
+                else
+                {
+
+                
+                SqlConnection con = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+                con.Open();
+                string gender = "M";
+                if (rbmale.Checked == true)
+                    gender = "M";
+                else
+                    gender = "F";
+
+                string str = " Insert into dewanshu_std(roll_no,name,father,state,gender) values ('" + txtrollno.Text + "', '" + txtname.Text + "', '" + txtfather.Text + "', '" + statecomb.SelectedItem.ToString() + "', '" + gender + "')";
+
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data saved");
+                fetchalldata();
+                    txtrollno.Text =  "" ;
+                    txtname.Text = "";
+                    txtfather.Text = "";
+                }
+            }
+            else
+                MessageBox.Show("sorry, student with same roll number already exists.");
+
+        
+        }
+
+        private void statecomb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+            conn.Open();
+            string str=txtquery.Text;
+            SqlDataAdapter ada = new SqlDataAdapter(str, conn);
+            DataTable dttab = new DataTable();
+            ada.Fill(dttab);    
+            conn.Close();
+            dataGridView1.DataSource = dttab;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private bool checkexistance()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+            conn.Open();
+            string str = "Select * from dewanshu_std where roll_no='"+txtrollno+"'";
+            SqlDataAdapter ada = new SqlDataAdapter(str, conn);
+            DataTable dttab = new DataTable();
+            ada.Fill(dttab);
+            conn.Close();
+            if (dttab.Rows.Count >0 )
+                return true;
+            else return false;      
+        }
+        private void fetchalldata()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=10.0.0.13;Database=mcadotnet;uid=student;pwd=mca@123;");
+            conn.Open();
+            string str = "Select * from dewanshu_std order by name";
+            SqlDataAdapter ada = new SqlDataAdapter(str, conn);
+            DataTable dttab = new DataTable();
+            ada.Fill(dttab);
+            conn.Close();
+            listView1.Items.Clear();
+            if (dttab.Rows.Count > 0)
+            {
+                for (int i = 0; i < dttab.Rows.Count; i++)
+                {
+                    ListViewItem li = new ListViewItem(dttab.Rows[i].ItemArray[0].ToString());
+                    li.SubItems.Add(dttab.Rows[i].ItemArray[1].ToString());//name
+                    li.SubItems.Add(dttab.Rows[i].ItemArray[2].ToString());//father
+                    li.SubItems.Add(dttab.Rows[i].ItemArray[4].ToString());//state
+                    li.SubItems.Add(dttab.Rows[i].ItemArray[3].ToString());//gender
+                    listView1.Items.Add(li);
+
+                }
+            }
+
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+
+            fetchalldata();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            txtrollno.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            txtname.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtfather.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            if (listView1.SelectedItems[0].SubItems[3].Text=="M")
+                rbmale.Checked = true;
+                else
+                rbfemale.Checked = true;
+            statecomb.SelectedItem= listView1.SelectedItems[0].SubItems[4].Text;
+
+        }
+    }
+}
